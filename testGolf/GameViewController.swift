a052a62511a449663d79655ceae49541db336f9b
Jonah Witcig <jwitcig@gmail.com>
2016-12-20 13:49:14 -0600

    Converting to SpriteKit.

--- testGolf/GameViewController.swift
+++ testGolf/GameViewController.swift
@@ -8,27 +8,21 @@
 
 import UIKit
 import QuartzCore
-import SceneKit
+import SpriteKit
 
 class GameViewController: UIViewController {
     
-    var sceneView: SCNView {
-        return view as! SCNView
+    var sceneView: SKView {
+        return view as! SKView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // allows the user to manipulate the camera
-        sceneView.allowsCameraControl = true
+        sceneView.showsFPS = true
         
-        // show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // configure the view
         sceneView.backgroundColor = UIColor.black
         
-        // add a tap gesture recognizer
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
         sceneView.addGestureRecognizer(tapGesture)
     }
@@ -46,83 +40,17 @@ class GameViewController: UIViewController {
         let firstHole = initial.holeSet[0]
         
         // create a new scene
-        let scene = SCNScene(named: "art.scnassets/course\(firstHole).scn")!
-        sceneView.scene = scene
-
+        let scene = SKScene(fileNamed: "art.scnassets/course\(firstHole).scn")!
+        
+        
         // create and add a camera to the scene
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        sceneView.scene!.rootNode.addChildNode(cameraNode)
-        
-        // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
-        
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        sceneView.scene!.rootNode.addChildNode(lightNode)
-        
-        // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = .ambient
-        ambientLightNode.light!.color = UIColor.darkGray
-        sceneView.scene!.rootNode.addChildNode(ambientLightNode)
-        
-        // retrieve the ship node
-        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
-        
-        let ball = scene.rootNode.childNode(withName: "sphere", recursively: true)!
-        
-        let light1 = scene.rootNode.childNode(withName: "light1", recursively: true)!
-        
-        let box1 = scene.rootNode.childNode(withName: "box1", recursively: true)!
-        
-        let rotateBox = SCNAction.rotateBy(x: 20, y: 0, z: 0, duration: 3.0)
-        
-        box1.runAction(rotateBox)
-        
-        ball.physicsBody?.applyForce(SCNVector3(0,1,2), asImpulse: true)
-        
-        // animate the 3d object
-        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
+        let camera = SKCameraNode()
+        scene.camera = camera
+        scene.addChild(camera)
     }
     
     func handleTap(_ gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // check what nodes are tapped
-        let p = gestureRecognize.location(in: scnView)
-        let hitResults = scnView.hitTest(p, options: [:])
-        // check that we clicked on at least one object
-        if hitResults.count > 0 {
-            // retrieved the first clicked object
-            let result: AnyObject = hitResults[0]
-            
-            // get its material
-            let material = result.node!.geometry!.firstMaterial!
-            
-            // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
-            
-            // on completion - unhighlight
-            SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
-                
-                material.emission.contents = UIColor.black
-                
-                SCNTransaction.commit()
-            }
-            
-            material.emission.contents = UIColor.red
-            
-            SCNTransaction.commit()
-        }
+        let location = gestureRecognize.location(in: sceneView)
     }
     
     override var shouldAutorotate: Bool {
